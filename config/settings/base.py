eeba98f71ab320593db207d069ba596ecf48440a
kim-sangah <kimsangah_@naver.com>
2024-12-10 01:19:08 +0900

    Update base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -92,7 +92,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = "static/"
-
+STATIC_ROOT = BASE_DIR / "static"
 # Default primary key field type
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"  # 기본 키 필드
 
