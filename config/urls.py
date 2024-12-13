9badcb337e74091412a6f663ce22af051670e80a
namjimin3123832 <mimi3123832@gmail.com>
2024-12-13 19:24:08 +0900

    :construction_worker: docker compose 파일 및 github-actions.docker-compose.yml 작성

--- config/urls.py
+++ config/urls.py
@@ -12,4 +12,4 @@ urlpatterns = [
     path('api/accounts/', include('pic.accounts.urls')),
     path('api/places/', include('pic.places.urls')),
 
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
