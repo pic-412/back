b1e1de199a237a1c64ce94a4e3f265b319b85c88
sangah <ksa@sangahui-MacBookAir.local>
2025-03-20 22:47:03 +0900

    :construction_worker: 배포 환경 설정 변경

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,14 +1,18 @@
 from .base import *
 
 SECRET_KEY = env("PROD_SECRET_KEY")
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-# CORS_ORIGIN_WHITELIST = ("http://211.188.59.221/",)
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+
+
+# CORS_ORIGIN_WHITELIST = ("http://211.188.59.221/",)
 # CORS_ALLOWED_ORIGIN_REGEXES = [
 #     r"^https://\w+\.example\.com$",
 # ]
+
 CORS_ALLOW_METHODS = [
     'DELETE',
     'GET',
@@ -29,10 +33,35 @@ CORS_ALLOW_HEADERS = [
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": env("PROD_DB_NAME"),
-        "USER": env("PROD_DB_USER"),
-        "PASSWORD": env("PROD_DB_PASSWORD"),
-        "HOST": env("PROD_DB_HOST"),
-        "PORT": env("PROD_DB_PORT"),
+        "NAME": env("POSTGRES_DB"),
+        "USER": env("POSTGRES_USER"),
+        "PASSWORD": env("POSTGRES_PASSWORD"),
+        "HOST": env("POSTGRES_HOST"),
+        "PORT": env("POSTGRES_PORT"),
+    }
+}
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': False,
+        },
     }
 }
